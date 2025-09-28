52d0c94d0a6c91de1213935436dc3dccfb327d4d
Millind amb <millindamb4@gmail.com>
2025-09-28 14:43:46 +0530

    event handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 function App() {
-  const url="https://jsonplaceholder.typicode.com/posts";
-  const [data,setdata]=useState(null);
-  const [showbtn,setshowbtn]=useState(true);
-  const fetchdata=async()=>{
-      const rawdata=await fetch(url);
-      const impdata=await rawdata.json();
-      setdata(impdata);
-    }
+  const [name,setname]=useState("");
+  // const pressed=()=>{
+  //   alert("button is pressed");
+  // }
+  //when we use input then to take the value inside it we have to 
+  // to use a function that will call on change in input and will take the input
+  const inputchange=(e)=>{
+    setname(e.target.value);
+  }
+  //to handle multiple input only by using a single function
+  //using useState to define multiple variables
+  const [login,setlogin]=useState({email:"",phone:"",pass:""})
+  //the function which will get called on change by three different inputs
+  const multiplechanges=(e)=>{
+    //this will set the value of variable according to thier name in the login 
+    setlogin({...login,[e.target.name]:e.target.value})
+    console.log(login);
+  }
   return (<>
-  <div className='cards'>
-    {data && data.map(detail => (
-          <div key={detail.title} className='card'>
-            <div className='ids'><span>User ID:{detail.userId}</span><span>ID :{detail.id}</span></div>
-            <div>Title:</div>
-            <div>{detail.title}</div>
-          </div>
-        ))}
-     {showbtn && <button className='btn' onClick={()=>{
-      setshowbtn(!showbtn);fetchdata()}
-     }>get data</button>}
-  </div>
+      <h1>Event handling in react</h1>
+      {/* <button onClick={pressed}>click this </button>
+      <button onMouseOver={pressed}>click this </button> */}
+      <input type="text" value={name} onChange={inputchange}/>
+      <button onClick={()=>{setname("")}}>submit</button><br/>
+      <input type="text" name="email" value={login.email} onChange={multiplechanges}/><br/>
+      <input type="text" name="phone" value={login.phone} onChange={multiplechanges}/><br/>
+      <input type="text" name="pass" value={login.pass} onChange={multiplechanges}/>
     </>
   )
 }
