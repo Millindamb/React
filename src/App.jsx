430d39b4c835ea48693ac6fa1132d2dde69e4740
Millind amb <millindamb4@gmail.com>
2025-10-04 13:45:37 +0530

    passwordgenerator

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState , useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [color,setColor]=useState('grey');
+  const [length,setlength]=useState(8);
+  const [ischar,setChar]=useState(false);
+  const [isnum,setNum]=useState(false);
+  const [password,setPassword]=useState("");
+  const passwordRef=useRef(null);
+  const passwordGenerator=useCallback(()=>{
+    let pass="";
+    let values="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdegjhijklmnopqrstuvwxyz";
+    if(ischar)values+="@#$%^&*!=+{}[]()/?~`"
+    if(isnum)values+="0123456789"
+    for(let i=1;i<=length;i++){
+      pass+=values.charAt(Math.floor(Math.random()*values.length));
+    }
+    setPassword(pass)
+    },[ischar,isnum,length,setPassword]
+  )
+  const copypasstoclipboard=useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password]);
+  useEffect(()=>{passwordGenerator()},[ischar,isnum,length]);
   return (
-    <div
-  style={{
-    backgroundColor: color,
-    height: "100vh",
-    width: "100vw"
-  }}>
-      <div className='Buttons'>
-        <button onClick={()=>{setColor('black')}}>black</button>
-        <button onClick={()=>{setColor('yellow')}}>yellow</button>
-        <button onClick={()=>{setColor('red')}}>red</button>
-        <button onClick={()=>{setColor('blue')}}>blue</button>
-        <button onClick={()=>{setColor('purple')}}>purple</button>
-        <button onClick={()=>{setColor('lightpink')}}>pink</button>
-        <button onClick={()=>{setColor('lightcyan')}}>cyan</button>
-        </div>
+    <div className='container'>
+    <div className='passgencontainer'>
+      <h2>Password Generator</h2>
+      <div className='mainpart'>
+        <input type="text" value={password} readOnly placeholder={password} ref={passwordRef}/>
+        <button onClick={copypasstoclipboard}>copy</button>
+      </div>
+      <div className='selection'>
+        <input type="range" min={6} max={24} value={length} onChange={(e)=>{setlength(e.target.value)}}/>len:{length}
+        <label><input type="checkbox" defaultChecked={isnum} onChange={()=>{setNum(!isnum)}}/>Numbers</label>
+        <label><input type="checkbox" defaultChecked={ischar} onChange={()=>{setChar(!ischar)}}/>Character</label>
+      </div>
+    </div>
     </div>
   )
 }
